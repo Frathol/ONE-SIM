a291ef7efb5630bb62c2244ae36847a3635d98ff
Frathol <michaelmosses35746@gmail.com>
2026-04-20 07:21:39 +0700

    Make QueuePolicy with enum

--- src/routing/UniversalQueueRouter.java
+++ src/routing/UniversalQueueRouter.java
@@ -10,131 +10,109 @@ import core.Settings;
 public class UniversalQueueRouter extends ActiveRouter {
   public static final String QUEUE_POLICY_S = "queuePolicy";
 
-  public static final String POLICY_FIFO = "FIFO";
-  public static final String POLICY_MOFO = "MOFO";
-  public static final String POLICY_MOPR = "MOPR";
-  public static final String POLICY_SHLI = "SHLI";
-  public static final String POLICY_LEPR = "LEPR";
+  public enum QueuePolicy {
+    FIFO, MOFO, MOPR, SHLI, LEPR
+  }
 
-  protected String currentPolicy;
+  // public static final String POLICY_FIFO = "FIFO";
+  // public static final String POLICY_MOFO = "MOFO";
+  // public static final String POLICY_MOPR = "MOPR";
+  // public static final String POLICY_SHLI = "SHLI";
+  // public static final String POLICY_LEPR = "LEPR";
+
+  protected QueuePolicy currentPolicy;
 
   public UniversalQueueRouter(Settings s) {
-    super(s);
-    if (s.contains(QUEUE_POLICY_S)) {
-      currentPolicy = s.getSetting(QUEUE_POLICY_S);
-    } else {
-      currentPolicy = POLICY_FIFO;
-    }
-  }
-
-  protected UniversalQueueRouter(UniversalQueueRouter r) {
-    super(r);
-    this.currentPolicy = r.currentPolicy;
-  }
-
-  @Override
-  protected boolean makeRoomForMessage(int size) {
-    if (size > this.getBufferSize()) {
-      return false;
+        super(s);
+        if (s.contains(QUEUE_POLICY_S)) {
+            String policyStr = s.getSetting(QUEUE_POLICY_S).toUpperCase();
+            try {
+                // Konversi String dari .cfg menjadi Enum constant
+                currentPolicy = QueuePolicy.valueOf(policyStr);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Warning: Unknown policy '" + policyStr + "', using FIFO.");
+                currentPolicy = QueuePolicy.FIFO;
+            }
+        } else {
+            currentPolicy = QueuePolicy.FIFO;
+        }
     }
 
-    int freeBuffer = this.getFreeBufferSize();
-    while (freeBuffer < size) {
-      Message m = getVictimMessage();
-      if (m == null)
-        return false;
-
-      deleteMessage(m.getId(), true);
-      freeBuffer += m.getSize();
+    protected UniversalQueueRouter(UniversalQueueRouter r) {
+        super(r);
+        this.currentPolicy = r.currentPolicy;
     }
-    return true;
-  }
 
-  private Message getVictimMessage() {
-    Collection<Message> messages = this.getMessageCollection();
-    Message victim = null;
+    @Override
+    protected boolean makeRoomForMessage(int size) {
+        if (size > this.getBufferSize()) return false;
 
-    for (Message m : messages) {
-      if (isSending(m.getId())) {
-        continue;
-      }
+        int freeBuffer = this.getFreeBufferSize();
+        while (freeBuffer < size) {
+            Message m = getVictimMessage();
+            if (m == null) return false;
 
-      if (victim == null) {
-        victim = m;
-        continue;
-      }
-
-      switch (currentPolicy) {
-        case POLICY_SHLI: // Shortest Life Time (TTL terendah)
-          if (m.getTtl() < victim.getTtl()) {
-            victim = m;
-          }
-          break;
-
-        case POLICY_FIFO: // First In First Out
-          if (m.getReceiveTime() < victim.getReceiveTime()) {
-            victim = m;
-          }
-          break;
-
-        case POLICY_MOFO: // Most Forwarded First (Hop count tertinggi)
-          if (m.getHopCount() > victim.getHopCount()) {
-            victim = m;
-          }
-          break;
-
-        case POLICY_MOPR: // Most Favorably Forwarded (FP Value tertinggi)
-          double mFP = getFPValue(m);
-          double victimFP = getFPValue(victim);
-          if (mFP > victimFP) {
-            victim = m;
-          }
-          break;
-
-        case POLICY_LEPR: // Least Probable First (P value terendah ke tujuan)
-          double mP = getPredictabilityToDestination(m.getTo());
-          double victimP = getPredictabilityToDestination(victim.getTo());
-          if (mP < victimP) {
-            victim = m;
-          }
-          break;
-
-        default: // Default kembali ke FIFO
-          if (m.getReceiveTime() < victim.getReceiveTime()) {
-            victim = m;
-          }
-          break;
-      }
+            deleteMessage(m.getId(), true);
+            freeBuffer += m.getSize();
+        }
+        return true;
     }
-    return victim;
-  }
 
-  // Helper untuk mengambil nilai FP (untuk MOPR) dari properti pesan
-  protected double getFPValue(Message m) {
-    Object prop = m.getProperty("FP_VALUE");
-    return (prop != null) ? (Double) prop : 0.0;
-  }
+    private Message getVictimMessage() {
+        Collection<Message> messages = this.getMessageCollection();
+        Message victim = null;
 
-  // Helper untuk mengambil nilai P (untuk LEPR)
-  // Fungsi ini akan di-override oleh Prophet nanti
-  protected double getPredictabilityToDestination(DTNHost destination) {
-    return 0.0;
-  }
+        for (Message m : messages) {
+            if (isSending(m.getId())) continue;
 
-  @Override
-  public void update() {
-    super.update();
-    // if (isTransferring() || !canStartTransfer())
-    //   return;
+            if (victim == null) {
+                victim = m;
+                continue;
+            }
 
-    // if (exchangeDeliverableMessages() != null)
-    //   return;
+            switch (currentPolicy) {
+                case SHLI: // Shortest Life Time (TTL terendah) 
+                    if (m.getTtl() < victim.getTtl()) victim = m;
+                    break;
 
-    // this.tryAllMessagesToAllConnections();
-  }
+                case MOFO: // Most Forwarded First (Hop count tertinggi) 
+                    if (m.getHopCount() > victim.getHopCount()) victim = m;
+                    break;
 
-  @Override
-  public UniversalQueueRouter replicate() {
-    return new UniversalQueueRouter(this);
-  }
+                case MOPR: // Most Favorably Forwarded (FP Value tertinggi) 
+                    if (getFPValue(m) > getFPValue(victim)) victim = m;
+                    break;
+
+                case LEPR:  // Least Probable First (P value terendah ke tujuan) 
+                    if (getPredictabilityToDestination(m.getTo()) < getPredictabilityToDestination(victim.getTo()))
+                        victim = m;
+                    break;
+
+                case FIFO: // First In First Out 
+                default:
+                    if (m.getReceiveTime() < victim.getReceiveTime()) victim = m;
+                    break;
+            }
+        }
+        return victim;
+    }
+
+    protected double getFPValue(Message m) {
+        Object prop = m.getProperty("FP_VALUE");
+        return (prop != null) ? (Double) prop : 0.0;
+    }
+
+    protected double getPredictabilityToDestination(DTNHost destination) {
+        return 0.0;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Override
+    public UniversalQueueRouter replicate() {
+        return new UniversalQueueRouter(this);
+    }
 }
