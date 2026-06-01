1ef3c1f238fff48b810c134b3b9cd629e27ff7dc
Frathol <michaelmosses35746@gmail.com>
2026-06-01 17:01:51 +0700

    Testing report for taking notes on message path

---
+++ src/report/MessagePathReport.java
@@ -0,0 +1,79 @@
+package report;
+
+import java.util.List;
+import core.DTNHost;
+import core.Message;
+import core.MessageListener;
+
+/**
+ * Custom Report untuk melacak jalur rute (Routing Path) dan jumlah Hop.
+ * Hanya mencatat pesan yang sukses sampai ke final destination.
+ */
+public class MessagePathReport extends Report implements MessageListener {
+
+  public MessagePathReport() {
+    init(); // Memanggil inisialisasi file saat objek dibuat
+  }
+
+  @Override
+  protected void init() {
+    super.init();
+    // Mencetak Header Tabel di baris pertama file CSV/TXT
+    write("=== MESSAGE PATH TRACER REPORT ===");
+    write(String.format("%-15s %-15s %-10s %s", "SimTime", "MessageID", "HopCount", "RoutingPath"));
+  }
+
+  @Override
+  public void messageTransferred(Message m, DTNHost from, DTNHost to, boolean firstDelivery) {
+    if (isWarmupID(m.getId())) {
+      return;
+    }
+
+    if (firstDelivery == true) {
+
+      List<DTNHost> jalur = m.getHops();
+
+      int hopCount = jalur.size() - 1;
+
+      StringBuilder ruteString = new StringBuilder();
+      for (int i = 0; i < jalur.size(); i++) {
+        ruteString.append(jalur.get(i).toString());
+        if (i < jalur.size() - 1) {
+          ruteString.append(" -> ");
+        }
+      }
+
+      String row = String.format("%-15.2f %-15s %-10d %s",
+          getSimTime(),
+          m.getId(),
+          hopCount,
+          ruteString.toString());
+
+      write(row);
+    }
+  }
+
+  @Override
+  public void newMessage(Message m) {
+    if (isWarmup()) {
+      addWarmupID(m.getId());
+    }
+  }
+
+  @Override
+  public void messageTransferStarted(Message m, DTNHost from, DTNHost to) {
+  }
+
+  @Override
+  public void messageDeleted(Message m, DTNHost where, boolean dropped) {
+  }
+
+  @Override
+  public void messageTransferAborted(Message m, DTNHost from, DTNHost to) {
+  }
+
+  @Override
+  public void done() {
+    super.done();
+  }
+}
